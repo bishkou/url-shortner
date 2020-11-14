79a7d819935ef0775c97d0fec5314f922a84da8a
bishkou <chedyhammami@yahoo.com>
2020-11-14 23:54:55 +0100

    npm start

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,8 @@ mongoose.connect('mongodb+srv://fb:fb@fb-mern.pgwy7.mongodb.net/fb?retryWrites=t
     console.log('connection failed');
   });
 
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 app
   .use(bodyParser.urlencoded({extended: true}))
